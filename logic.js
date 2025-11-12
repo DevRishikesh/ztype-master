fa8a8282e474582a2713fc0cb630f08f7647efe3
Rishikesh Ragav <147292837+DevRishikesh@users.noreply.github.com>
2025-11-12 16:55:45 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -221,19 +221,18 @@ const state = {
   },
 };
 
-// Selecting most used / important dom elements
+ 
 const wordsContainer = document.querySelector(".words-container");
-// Hidden mobile input to detect user input on virtual keyboards
+ 
 const mobileInput = document.getElementById("mobileInput");
 
 function addfilter(event) {
-  // implementing
-  // listen to user input and update the filters object
+ 
   let target = event.target;
   let filter = target.dataset.filter;
   let currentValue = filters[filter];
 
-  // Punctuation & Numbers Filter
+   
   if (currentValue == true) {
     filters[filter] = false;
   } else {
@@ -248,7 +247,7 @@ function addfilter(event) {
   updateCursor();
 }
 
-// Handle Time Filter
+ 
 function timeFilter(event) {
   const timeDiv = document.querySelector("#time-container");
   let target = event.target;
@@ -268,25 +267,25 @@ function timeFilter(event) {
   updateCursor();
 }
 
-// Function to update the words in UI, can take a parameter of puncuation (true or false)
+ 
 function updateWords() {
   const { punctuation, numbers } = filters;
   const noSentences = 15;
-  wordsContainer.innerHTML = ""; // Deleting previous words from ui
+  wordsContainer.innerHTML = ""; 
   for (let i = 0; i < noSentences; i++) {
     let sentence = makeSentence(punctuation, numbers);
-    // Looping over each word in sentence
+    
     sentence.forEach((word, index) => {
       word = String(word);
       let wordDiv = document.createElement("div");
       wordDiv.className =
         "word font-normal text-secondary/20 mr-2 mb-2 md:mr-3 mb-3 select-none	";
 
-      // Make the first word active by adding a data attribute
+       
       if (index === 0) {
         wordDiv.dataset.active = "true";
       }
-      // Looping over each character in word
+    
       for (let i = 0; i < word.length; i++) {
         let charSpan = document.createElement("span");
         charSpan.classList.add("transition-colors", "duration-200");
@@ -298,42 +297,35 @@ function updateWords() {
   }
   let activeCharacter = wordsContainer.children[0].children[0];
 
-  // Update Global State
+  
   state.word = wordsContainer.children[0];
   state.letter = state.word.children[0];
   state.isWord = true;
   state.letter.scrollIntoView();
 }
-
-// Function to make a unique sentence with at option to include puncuations or numbers as well
-// punctuation and numbers paratameters can be true or false
+ 
 function makeSentence(punctuation, numbers) {
   let sentence = [];
   let sentenceLength = getRandomIntInclusive(4, 10);
-
-  // Loop random sentence length many times & add words to the sentence array.
+ 
   for (let i = 0; i <= sentenceLength; i++) {
     let word = words[getRandomIntInclusive(0, words.length - 1)];
     sentence.push(word);
   }
-
-  // If user wants punctuation
+ 
   if (punctuation == true) {
-    // Captalize the first letter of the first word
+  
     let firstWord = sentence[0];
     sentence[0] = capatalize(firstWord);
 
     let lastWord = sentence[sentence.length - 1];
     sentence[sentence.length - 1] += ".";
-
-    // Adding a symbol at the end of the current word at a random index
+ 
     let randomIndex = getRandomIntInclusive(0, sentenceLength - 1);
     let randomWord = sentence[randomIndex];
     sentence[randomIndex] = addSymbolAtEnd(randomWord);
   }
-
-  // If user wants numbers then
-  // add at least one number in the sentence
+ 
   if (numbers == true) {
     let randomIndex = getRandomIntInclusive(1, sentenceLength - 1);
     let randomNumber = getRandomIntInclusive(0, 999);
@@ -342,10 +334,7 @@ function makeSentence(punctuation, numbers) {
 
   return sentence;
 }
-
-// Helper Functions Below
-
-// Capatalize the string and return it
+ 
 function capatalize(string) {
   let newString = "";
   for (let i = 0; i < string.length; i++) {
@@ -360,8 +349,7 @@ function capatalize(string) {
 
   return newString;
 }
-
-// Wrap the string with quotations and return it
+ 
 function wrapWithQuotations(string) {
   let newString = "";
   let quotations = ["'", '"'];
@@ -371,8 +359,7 @@ function wrapWithQuotations(string) {
   newString += randomQuote;
   return newString;
 }
-
-// Add a random symbol at the end of string and return it
+ 
 function addSymbolAtEnd(string) {
   let symbols = [",", ":", ";", " -", "!"];
   let randomSymbol = symbols[getRandomIntInclusive(0, symbols.length - 1)];
@@ -380,27 +367,23 @@ function addSymbolAtEnd(string) {
   return string;
 }
 
-// Getting a random integer between two values, inclusive
-// MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+ 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min); 
 }
 
 function userTypes(event) {
   document.querySelector("#mobileInput").value = event.key;
-  // Our program to assume that there is only single word.
-  // Once we are done with our current word, update the current word to be next word.
-  // Error, mistypes
-
+ 
   if (!isValidTypingKey(event)) {
     return;
   }
 
-  // Update global allt typed entries
+ 
   state.allLettersTyped++;
-  // The number of correct words user typed.
+  
   if (!state.timerRunning) {
     startCountdown();
   }
@@ -409,7 +392,7 @@ function userTypes(event) {
   const userLetter = event.key;
 
   if (!state.isWord) {
-    // If user types space, move to next word.
+ 
     if (event.key == " ") {
       event.preventDefault();
       state.word = state.word.nextSibling;
@@ -418,7 +401,7 @@ function userTypes(event) {
       updateCursor();
       state.correctLettersTyped++;
     } else {
-      // user typed an extra character
+     
       state.errors += 1;
       let extraCharacter = document.createElement("span");
       extraCharacter.classList.add("extra", "text-red-900");
@@ -431,14 +414,14 @@ function userTypes(event) {
   }
 
   if (letter.innerText == userLetter) {
-    // user typed correct letter
+     
     letter.classList.add("correct");
     if (letter.nextSibling) {
       state.letter = letter.nextSibling;
     }
     state.correctLettersTyped++;
   } else {
-    // user typed wrong letter
+   
     state.errors += 1;
     letter.classList.add("wrong");
     if (state.letter.nextSibling) {
@@ -446,21 +429,21 @@ function userTypes(event) {
     }
     updateCursor();
   }
-  // Check if current letter is the last letter of the word
+  
   if (!letter.nextSibling) {
     state.isWord = false;
   }
   updateCursor();
 }
 
-// Functionality of deleting a character
+ 
 function backspace(event) {
-  // if first letter of the first word, return
+ 
   if (!state.word.previousSibling && !state.letter.previousSibling) {
     return;
   }
 
-  // first character of the any word  and last character of the previous word is wrong
+ 
   if (!state.letter.previousSibling && state.word.previousSibling) {
     const prevWord = state.word.previousSibling;
     const prevWordLastChar = prevWord.children[prevWord.childElementCount - 1];
@@ -470,7 +453,7 @@ function backspace(event) {
     ) {
       state.letter = prevWordLastChar;
       state.word = prevWord;
-      // Check if current letter is the last letter of the word
+    
       if (!state.letter.nextSibling) {
         state.isWord = false;
       }
@@ -479,7 +462,7 @@ function backspace(event) {
     return;
   }
 
-  // last character of the word
+ 
   if (
     state.letter.className.includes("correct") ||
     state.letter.className.includes("wrong")
@@ -490,7 +473,7 @@ function backspace(event) {
     return;
   }
 
-  // if extra character
+   
   if (state.letter.classList.contains("extra")) {
     let tempLetter = state.letter;
     state.letter = state.letter.previousSibling;
@@ -507,12 +490,11 @@ function backspace(event) {
 function updateCursor() {
   let letterRect = state.letter.getBoundingClientRect();
 
-  // Subtracting Distance of viewport to the top of the wordsContainer from top of the viewport to the top of cursor gives us the position of the cursor relative to the wordsContainer Div
+   
   const wordsContainerRect = wordsContainer.getBoundingClientRect();
   let cursorPositionRelativeToWordsContainer =
     letterRect.top - wordsContainerRect.top;
-  // If current words is in the middle of the wordsContainer div then scroll into view
-  // i.e scroll the current word into view if the user is typing at least on the middle line of the text
+ 
   if (cursorPositionRelativeToWordsContainer >= wordsContainerRect.height / 2) {
     wordsContainer.scroll({
       top: wordsContainer.scrollTop + wordsContainerRect.height / 3,
@@ -531,7 +513,7 @@ function updateCursor() {
   ) {
     cursorDiv.style.left = `${letterRect.left + letterRect.width}px`;
   }
-  // Make cursor visible on first page load
+ 
   cursorDiv.classList.remove("invisible");
 }
 
@@ -540,7 +522,7 @@ function startCountdown() {
   let timeLeft = filters.time;
   let timer = setInterval(() => {
     const timeContainer = document.querySelector("#time-container");
-    // Restart test without waiting for results
+ 
     if (state.timerRunning === false) {
       displayUI(false);
       clearInterval(timer);
@@ -561,7 +543,7 @@ function startCountdown() {
   }, 1000);
 }
 
-// displayUI -> display results or normal wordsContainer depending on the showResults parameter
+ 
 function displayUI(showResults) {
   const timeContainer = document.querySelector("#time-container");
   const resultsContainer = document.querySelector(".results-container");
@@ -588,25 +570,25 @@ function displayUI(showResults) {
 function calculateWPM() {
   const time = filters.time / 60;
   const uncorrected = state.uncorrectedErrors;
-  let gpm = (state.allLettersTyped / 5) / time // prettier-ignore
-  let netWPM = gpm - (uncorrected / time); // prettier-ignore
+  let gpm = (state.allLettersTyped / 5) / time  
+  let netWPM = gpm - (uncorrected / time); 
   if (netWPM < 0) {
     netWPM = 0;
   }
-  // reset user typing stats in state
+ 
   state.uncorrectedErrors = 0;
   return Math.floor(netWPM);
 }
 
 function calculateAccuracy() {
   let accuracy = (state.correctLettersTyped / state.allLettersTyped) * 100;
-  // reset user typing stats in state
+ 
   state.correctLettersTyped = 0;
   state.allLettersTyped = 0;
   return Math.floor(accuracy);
 }
 
-// Function to check all the non corrector errors after the test time finishes
+ 
 function evaluateNonCorrectedErrors() {
   let words = document.querySelectorAll(".word");
 
@@ -626,21 +608,21 @@ function evaluateNonCorrectedErrors() {
 
 function handleFiltersPersists() {
   if (localStorage.getItem("filters")) {
-    // If filters exist already, update program filters to be the one as localstorage
+  
     filters = JSON.parse(localStorage.getItem("filters"));
   } else {
-    // If no filters in localstorage, then add program's current filters to localstorage
+    
     localStorage.setItem("filters", JSON.stringify(filters));
   }
 
-  // Selecting all filters componenets from UI
+ 
   const timeFilters = document.querySelectorAll("[data-filter='time']");
   const punctuationFilter = document.querySelector(
     "[data-filter='punctuation']"
   );
   const numbersFilter = document.querySelector("[data-filter='numbers']");
 
-  //  Updating the timer filters in UI according to the filters stored in localstorage
+ 
   timeFilters.forEach((filter) => {
     const value = filter.dataset.time;
     if (filters.time == value) {
@@ -650,14 +632,14 @@ function handleFiltersPersists() {
     }
   });
 
-  //  Updating the punctuation filter in UI according to the filters stored in localstorage
+ 
   if (filters.punctuation === true) {
     punctuationFilter.classList.add("active-tool-btn");
   } else {
     punctuationFilter.classList.remove("active-tool-btn");
   }
 
-  //  Updating the numbers filter in UI according to the filters stored in localstorage
+ 
   if (filters.numbers === true) {
     numbersFilter.classList.add("active-tool-btn");
   } else {
@@ -665,8 +647,7 @@ function handleFiltersPersists() {
   }
   document.querySelector("#time-container").innerText = filters.time;
 }
-
-// Resets the filter to the initial state (time = 30s, everything else false)
+ 
 function resetFilters() {
   focusWordsDiv();
   let resetFilters = {
@@ -675,7 +656,7 @@ function resetFilters() {
     numbers: false,
   };
   localStorage.setItem("filters", JSON.stringify(resetFilters));
-  // Stop timer
+ 
   state.timerRunning = false;
   state.timerFinished = true;
   handleFiltersPersists();
@@ -683,40 +664,37 @@ function resetFilters() {
   updateWords();
 }
 
-// Remove the focus from whatever element is currently focused and focus on the words div.
+ 
 function focusWordsDiv() {
   document.activeElement.blur();
   wordsContainer.focus();
 }
-
-// Restart the test when clicked on restart button in UI
+ 
 function restartTest() {
   const resultsContainer = document.querySelector(".results-container");
   const timeContainer = document.querySelector("#time-container");
   const cursorDIV = document.querySelector(".cursor");
 
   if (!state.timerRunning && resultsContainer.classList.contains("hidden")) {
-    // Dont restart test if its not started yet and results are there
+    
     return;
   }
-
-  // Check if results Div is hidden, if it is, just update the ui normally
+ 
   if (resultsContainer.classList.contains("hidden")) {
     state.timerFinished = true;
     state.timerRunning = false;
     focusWordsDiv();
     return;
   }
-  // otherwise, hide resutls and then display
+ 
   resultsContainer.classList.add("hidden");
   timeContainer.classList.toggle("invisible");
   cursorDIV.classList.remove("hidden");
   displayUI(false);
   focusWordsDiv();
-  // state.timerRunning = false;
+  
 }
-
-// Function to check if user pressed a valid key to type, i.e avoid keys like ctrl, shift etc
+ 
 function isValidTypingKey(event) {
   const invalidKeys = [
     "Tab",
@@ -735,7 +713,7 @@ function isValidTypingKey(event) {
   }
   return true;
 }
-// Render the UI on first page load
+ 
 document.addEventListener("DOMContentLoaded", () => {
   handleFiltersPersists();
   const { punctuation, numbers } = filters;
@@ -743,11 +721,9 @@ document.addEventListener("DOMContentLoaded", () => {
   focusWordsDiv();
   updateCursor();
 });
-
-// When words div is in focus, listen to the keydown events
+ 
 document.onkeydown = function (event) {
-  // because events capture and bubble, we dont want this function to execute if clicked on any relevant buttons such as filters or restart buttons
-  // If device is a mobile or table, return
+ 
   if (isMobileOrTablet()) {
     return;
   }
@@ -758,10 +734,7 @@ document.onkeydown = function (event) {
   ) {
     return;
   }
-
-  // if (state.timerFinished) {
-  //   return;
-  // }
+ 
 
   if (event.key === "Backspace") {
     backspace(event);
@@ -770,33 +743,33 @@ document.onkeydown = function (event) {
   if (!isValidTypingKey(event)) {
     return;
   }
-  // Focus the words container if the device is not a mobile or table
+   
   focusWordsDiv();
 };
 
-// Handle user typing event
+ 
 wordsContainer.onkeydown = (event) => {
-  // Disable scrolling on wordsContainer with space key
+ 
   if (event.code == "Space") {
     event.preventDefault();
   }
   userTypes(event);
 };
 
-// Handle user typing for touchscreens
+ 
 wordsContainer.onclick = () => {
-  // Dont do anything on non touch devices
+  
   if (!isMobileOrTablet()) {
     return;
   }
-  // Focus on the hidden input tag when clicked
+ 
   document.activeElement.blur();
   mobileInput.focus();
   mobileInput.value = "@";
   return;
 };
 
-// Grabbing user typed key on touch screens / virutal keyboards
+ 
 mobileInput.onkeyup = () => {
   const userInput = mobileInput.value;
   const event = {
@@ -805,70 +778,66 @@ mobileInput.onkeyup = () => {
       return;
     },
   };
-  // Handle backspace
+ 
   if (userInput === "") {
     backspace();
     mobileInput.value = "@";
     return;
   }
-  // Handle any other input
+ 
   event.key = userInput[1];
   userTypes(event);
-  // Reset the input
+ 
   mobileInput.value = "@";
 };
-// Listen to the scroll event
-// update the letter Rect accordingly
-// When scrolled inside wordsContainer div, update the cursor.
+ 
 wordsContainer.onscroll = () => {
   let timer = null;
   if (timer !== null) {
     clearTimeout(timer);
   }
   timer = setTimeout(function () {
-    // update the letter Rect accordingly
+    
     updateCursor();
   }, 200);
 };
 
-// Update cursor when window resizes because it was not working properly without it
+ 
 window.onresize = () => {
   updateCursor();
 };
 
-// Disable scrolling on wordsContainer with mouse
+ 
 wordsContainer.onwheel = (event) => {
   event.preventDefault();
 };
-
-// Change theme function, attached to color input in the header
+ 
 function changeTheme(event) {
-  // Get RGB channels of hexadecimal color
+ 
   const color = event.target.value;
   let HexR = color.slice(1, 3);
   let HexG = color.slice(3, 5);
   let HexB = color.slice(5, 7);
-  // Convert hexadecimal RGB channels into decimal rgb channels
+ 
   let decR = parseInt(HexR, 16);
   let decG = parseInt(HexG, 16);
   let decB = parseInt(HexB, 16);
   let accentColor = `${decR} ${decG} ${decB}`;
-  // Update the css variable in order to change colors
+ 
   let root = document.documentElement;
   root.style.setProperty("--color-secondary", accentColor);
 }
 
-// Store the selected color in localStorage after user selects color and dismisses the input
+ 
 function storeTheme() {
   const currentColor = getComputedStyle(
     document.documentElement
   ).getPropertyValue("--color-secondary");
 
-  // store color in local storage
+ 
   localStorage.setItem("color-secondary", currentColor);
 }
-// Detect mobile browser
-// https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
+ 
 const isMobileOrTablet = () => {
   let check = false;
   (function (a) {
